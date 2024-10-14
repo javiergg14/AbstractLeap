d7782bf50dddbd2d837b6fb1ae82e059c879fd45
Javier Gómez González <javigg011205@gmail.com>
2024-10-14 10:05:06 +0200

    Mapa

---
+++ Output/Assets/Maps/AbstractLeap.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="AbstractLeap" tilewidth="32" tileheight="32" tilecount="1056" columns="44">
+ <image source="../Documents/GitHub/AbstractLeap/Output/Assets/textures/Tilesheet/tilesheet_complete.png" width="1408" height="768"/>
+</tileset>
